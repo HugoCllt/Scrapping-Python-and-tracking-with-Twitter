4213f4e878a91079d1af059be77611eb8062e5f9
Hugo Cllt <hugocoeuillet@gmail.com>
2021-10-18 21:18:21 +0200

    Twitter link and Date have been added [HC]

---
+++ main.py
@@ -0,0 +1,28 @@
+import bs4
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://howrare.is/drops'
+html = requests.get(url)
+soup = BeautifulSoup(html.text, 'html.parser')
+
+tableau = soup.find('table', class_='table')
+
+for t in tableau.find_all('tbody'):
+    rows = t.find_all('tr')
+    for row in rows:
+        name = row.findAll('td', class_='')[0].text.strip()
+        time = row.findAll('td', class_='')[2].text.strip()
+        count = row.findAll('td', class_='')[4].text.strip()
+        price = row.findAll('td', class_='')[5].text.strip().split(' ')[0]
+        print(f'''
+              name : {name}
+              time : {time}
+              count : {count}
+              price : {price}
+              ''')
+
+
+    
+    
+    
