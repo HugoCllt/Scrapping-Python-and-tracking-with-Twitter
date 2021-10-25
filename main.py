180a65a855c439384e9e95ea4541321cbed932c3
vicfeuga <50053627+vicfeuga@users.noreply.github.com>
2021-10-25 10:17:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import bs4
 from bs4 import BeautifulSoup
 import requests
+import json
+from urllib.parse import urlparse
 
 
 url = 'https://howrare.is/drops'
-html = requests.get(url)
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36"}
+
+html = requests.get(url,headers=headers)
 soup = BeautifulSoup(html.text, 'html.parser')
 i = -1
 j = 0
 dicDate={}
+data=[]
 
 titles = soup.findAll('p', class_ = 'title')
 for title in titles:
@@ -23,24 +28,16 @@ for eacht in tableau:
     for t in eacht.findAll('tbody'):
         rows = t.findAll('tr')
     for row in rows:
+        d=dict()
+        d['name'] = row.findAll('td', class_='')[0].text.strip()
+        d['time'] = row.findAll('td', class_='')[2].text.strip()
+        d['count'] = row.findAll('td', class_='')[-3].text.strip()
+        d['price'] = row.findAll('td', class_='')[-2].text.strip().split(' ')[0]
+        d['lien'] = str(row.findAll('a')[-1]).split('href="')[1].split('" target')[0]
         
-        name = row.findAll('td', class_='')[0].text.strip()
-        time = row.findAll('td', class_='')[2].text.strip()
-        count = row.findAll('td', class_='')[-3].text.strip()
-        price = row.findAll('td', class_='')[-2].text.strip().split(' ')[0]
-        liens = row.findAll('td', class_='')[1]
-        lien = str(row.findAll('a')[-1]).split('href="')[1].split('" target')[0]
+        d['date'] = dicDate[i]
         
-        date = dicDate[i]
-        
-        print(f'''
-              name : {name}
-              liens : {lien}
-              time : {time}
-              count : {count}
-              price : {price}
-              date : {date}
-              ''')
-print(i)
-print(j)
-#compter le nombre de tableau, compter le nombre de date, assigner une date par tableau
+        data.append(d)
+
+with open('classement.json','w') as f:
+    json.dump(data,f,indent=4)
